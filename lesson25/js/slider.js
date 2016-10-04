0551ecc2fdda5709f7c0c7fdf5e5978f0b82b44b
mohong <arthas_qin@qq.com>
2016-10-04 22:31:59 +0800

    lesson25 scroll buttons generated

--- lesson25/js/slider.js
+++ lesson25/js/slider.js
@@ -1,3 +1,21 @@
 /**
  * Created by mohong on 2016/10/4.
  */
+window.onload = function () {
+    //封装id选择器
+    function $(id) {
+        return document.getElementById(id);
+    }
+    var js_slider = $("js_slider");
+    var slider_main_block = $("slider_main_block");
+   //获取所有的图片组
+    var imgs = slider_main_block.children;
+    var slider_ctrl = $("slider_ctrl");
+    //生成span
+    for(var i=0; i<imgs.length; i++){
+        var span = document.createElement("span");
+        span.className = "slider-ctrl-con";
+        span.innerHTML = imgs.length - i;
+        slider_ctrl.insertBefore(span,slider_ctrl.children[1]);
+    }
+}
